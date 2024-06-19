cfa08cd7b953be167bb6e5673b9a72e8559218b2
이남영 <cuocsong26496@gmail.com>
2024-06-19 08:22:18 +0700

    Update timefarm.py

--- timefarm.py
+++ timefarm.py
@@ -15,12 +15,12 @@ def parse_arguments():
 
     if args.task is None:
         # Jika parameter --upgrade tidak diberikan, minta input dari pengguna
-        task_input = input("Apakah Anda ingin auto claim task? (y/n, default n): ").strip().lower()
+        task_input = input("Bạn có muốn tự động nhận nhiệm vụ không? (y/n, mặc định n): ").strip().lower()
         # Jika pengguna hanya menekan enter, gunakan 'n' sebagai default
         args.task = task_input if task_input in ['y', 'n'] else 'n'
     
     if args.upgrade is None:
-        upgrade_input = input("Apakah Anda ingin auto upgrade clock? (y/n, default n): ").strip().lower()
+        upgrade_input = input("Bạn có muốn tự động nâng cấp đồng hồ không? (y/n, mặc định n): ").strip().lower()
         args.upgrade = upgrade_input if upgrade_input in ['y', 'n'] else 'n'
 
     return args
@@ -52,10 +52,10 @@ def get_access_token_and_info(query_data):
         response.raise_for_status()  # Akan memicu error jika status bukan 200
         return response.json()
     except json.JSONDecodeError:
-        print(f"JSON Decode Error: Query Anda Salah")
+        print(f"Lỗi giải mã JSON: Query của bạn sai")
         return None
     except requests.RequestException as e:
-        print(f"Request Error: {e}")
+        print(f"Lỗi yêu cầu: {e}")
         return None
 
 def cek_farming(token):
@@ -128,18 +128,18 @@ def auto_upgrade(token):
         response = upgrade_level(token)
         if 'error' in response:
             if response['error']['message'] == "Not enough balance":
-                print(Fore.RED + Style.BRIGHT + f"\r[ Upgrade ] : Tidak memiliki cukup saldo untuk upgrade.", flush=True)
+                print(Fore.RED + Style.BRIGHT + f"\r[ Nâng cấp ] : Không đủ số dư để nâng cấp.", flush=True)
                 break
             elif response['error']['message'] == "Forbidden":
-                print(Fore.RED + Style.BRIGHT + f"\r[ Upgrade ] : Error upgrade.", flush=True)
+                print(Fore.RED + Style.BRIGHT + f"\r[ Nâng cấp ] : Lỗi nâng cấp.", flush=True)
             elif response['error']['message'] == "Max level reached":
-                print(Fore.RED + Style.BRIGHT + f"\r[ Upgrade ] : Sudah mencapai level maksimal.", flush=True)
+                print(Fore.RED + Style.BRIGHT + f"\r[ Nâng cấp ] : Đã đạt cấp độ tối đa.", flush=True)
                 break
             else:
-                print(Fore.RED + Style.BRIGHT + f"\r[ Upgrade ] : Error upgrade. {response['error']['message']}", flush=True)
+                print(Fore.RED + Style.BRIGHT + f"\r[ Nâng cấp ] : Lỗi nâng cấp. {response['error']['message']}", flush=True)
                 break
         else:
-            print(Fore.GREEN + Style.BRIGHT + f"\r[ Upgrade ] : Upgrade berhasil, next..", flush=True)
+            print(Fore.GREEN + Style.BRIGHT + f"\r[ Nâng cấp ] : Nâng cấp thành công, tiếp theo..", flush=True)
 
 # Tambahkan pemanggilan fungsi ini di dalam loop utama jika pengguna memilih untuk auto upgrade
 
@@ -150,9 +150,9 @@ def animated_loading(duration):
     while time.time() < end_time:
         remaining_time = int(end_time - time.time())
         for frame in frames:
-            print(f"\rMenunggu waktu claim berikutnya {frame} - Tersisa {remaining_time} detik         ", end="", flush=True)
+            print(f"\rĐang chờ thời gian nhận tiếp theo {frame} - Còn lại {remaining_time} giây         ", end="", flush=True)
             time.sleep(0.25)
-    print("\rMenunggu waktu claim berikutnya selesai.                            ", flush=True)     
+    print("\rĐang chờ thời gian nhận tiếp theo hoàn thành.                            ", flush=True)     
 def print_welcome_message():
     print(r"""
           
@@ -160,158 +160,80 @@ def print_welcome_message():
 █▄█ █▀█ █▀█ █▄▄ █ █▄█ █ ██▄
           """)
     print(Fore.GREEN + Style.BRIGHT + "TimeFarm BOT")
-    print(Fore.CYAN + Style.BRIGHT + "Update Link: https://github.com/adearman/timefarm")
-    print(Fore.YELLOW + Style.BRIGHT + "Free Konsultasi Join Telegram Channel: https://t.me/ghalibie")
-    print(Fore.BLUE + Style.BRIGHT + "Buy me a coffee :) 0823 2367 3487 GOPAY / DANA")
-    print(Fore.RED + Style.BRIGHT + "NOT FOR SALE ! Ngotak dikit bang. Ngoding susah2 kau tinggal rename :)")
+    print(Fore.CYAN + Style.BRIGHT + "Liên kết cập nhật: https://github.com/adearman/timefarm")
+    print(Fore.YELLOW + Style.BRIGHT + "Credit: https://t.me/ghalibie")
     current_time = datetime.now()
     up_time = current_time - start_time
     days, remainder = divmod(up_time.total_seconds(), 86400)
     hours, remainder = divmod(remainder, 3600)
     minutes, seconds = divmod(remainder, 60)
-    print(Fore.CYAN + Style.BRIGHT + f"Up time bot: {int(days)} hari, {int(hours)} jam, {int(minutes)} menit, {int(seconds)} detik")
+    print(Fore.CYAN + Style.BRIGHT + f"Thời gian bot chạy: {int(days)} ngày, {int(hours)} giờ, {int(minutes)} phút, {int(seconds)} giây")
 
 def extract_user_details(query_line):
-    parts = query_line.split('&')
-    user_info_encoded = [part for part in parts if part.startswith('user=')][0]
-    user_info_encoded = user_info_encoded.split('=')[1]
-    user_info_json = urllib.parse.unquote(user_info_encoded)
-    user_info = json.loads(user_info_json)
-    return user_info.get('username', "Tidak Ada Username"), user_info.get('first_name', "Tidak Ada Firstname"), user_info.get('last_name', "Tidak Ada Lastname")
+    parts = query_line.split(' ')
+    token = parts[0]
+    user_id = parts[1]
+    username = parts[2]
+    return token, user_id, username
 
-def main():
-    while True:
+def read_query_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+def run_bot(file_path):
+    query_lines = read_query_file(file_path)
+    for query_line in query_lines:
+        token, user_id, username = extract_user_details(query_line)
         print_welcome_message()
-        try:
-            with open('query.txt', 'r') as file:
-                queries = file.readlines()
-            
-            for query_data in queries:
-                username, first_name, last_name = extract_user_details(query_data.strip())
-
-                query_data = query_data.strip()
-                auth_response = get_access_token_and_info(query_data)
-                # user_info = extract_user_info(query_data)
-                # print(user_info)
-                token = auth_response['token']
-
-                balance_info = auth_response['balanceInfo']
-
-                # username = balance_info.get('user', {}).get('userInfo', {}).get('userName', "Tidak Ada Username")
-                # first_name = balance_info.get('user', {}).get('userInfo', {}).get('firstName', "Tidak Ada Firstname")
-                # last_name = balance_info.get('user', {}).get('userInfo', {}).get('lastName', "Tidak Ada Lastname")
-                print(Fore.CYAN + Style.BRIGHT + f"\n===== [ {first_name} {last_name} | {username} ] =====")
-                print(Fore.YELLOW + Style.BRIGHT + f"[ Balance ] : {int(balance_info['balance']):,}".replace(',', '.'))
-                if cek_upgrade_enable == 'y':
-                    print(Fore.YELLOW + Style.BRIGHT + f"\r[ Upgrade ] : Upgrading Clock..", end="", flush=True)
-                    auto_upgrade(token)
-                if cek_task_enable == 'y':
-                    print(Fore.YELLOW + Style.BRIGHT + f"\r[ Task ] : Checking ...", end="", flush=True)
-                    tasks = cek_task(token)
-      
-                    if tasks:
-                        for task in tasks:
-                            # if "TimeFarm" in task['title']:
-                            #     continue  
-                            if task.get('submission', {}).get('status') == 'CLAIMED':
-                                print(Fore.GREEN + Style.BRIGHT + f"\r[ Task ] : {task['title']} | Claimed                                               ", flush=True)
-                            elif task.get('submission', {}).get('status') == 'COMPLETED':
-                                print(Fore.GREEN + Style.BRIGHT + f"\r[ Task ] : Claiming {task['title']}", flush=True)
-                                response = claim_task(token, task['id'])
-                                # print(response)
-                                if response is not None:
-                                    if 'error' in response:
-                                        if response['error']['message'] == "Failed to claim reward":
-                                            print(Fore.RED + Style.BRIGHT + f"\r[ Task ] : Claim task: {task['title']} | Already claimed", end="", flush=True)
-                                    else:
-                                        print(Fore.GREEN + Style.BRIGHT + f"\r[ Task ] : Claim task: {task['title']} | Claimed", flush=True)    
-                            
-                            else:
-                                print(f"\r[ Task ] : Submit task: {task['title']}", end="", flush=True)
-                                if task.get('submission', {}).get('status') == 'SUBMITTED':
-                                    print(Fore.YELLOW + Style.BRIGHT + f"\r[ Task ] : Submit task: {task['title']} | Already Submitted", flush=True)
-                                else:
-                                    response = submit_task(token, task['id'])
-                                    # print(response)
-                                    if response is not None:
-                                        if 'error' in response:
-                                            print(Fore.RED + Style.BRIGHT + f"\r[ Task ] : Submit task: {task['title']} | {response['error']['message']}", end="", flush=True)
-                                        else:
-                                            print(Fore.GREEN + Style.BRIGHT + f"\r[ Task ] : Submit task: {task['title']} | Submitted", flush=True)
-                                    time.sleep(3)  # Tunggu 3 detik sebelum mengklaim
-                                print(f"\r[ Task ] : Claim task: {task['title']}", end="", flush=True)
-                                response = claim_task(token, task['id'])
-                                # print(response)
-                                if response is not None:
-                                    if 'error' in response:
-                                        if response['error']['message'] == "Failed to claim reward":
-                                            print(Fore.RED + Style.BRIGHT + f"\r[ Task ] : Claim task: {task['title']} | Failed to claim reward / already claimed", end="", flush=True)
-                                    else:
-                                        print(Fore.GREEN + Style.BRIGHT + f"\r[ Task ] : Claim task: {task['title']} | Claimed", flush=True)
-                print(Fore.YELLOW + Style.BRIGHT + f"\r[ Farming ] : Checking ...", end="", flush=True)
-                time.sleep(2)
-                farming_response = finish_farming(token)
-                if farming_response is not None:
-                    if 'error' in farming_response:
-                        if farming_response['error']['message'] == "Too early to finish farming":
-
-                            cek_farming_response = cek_farming(token)
-                            if cek_farming_response:
-                                started_at = datetime.fromisoformat(cek_farming_response['activeFarmingStartedAt'].replace('Z', '+00:00')).astimezone(timezone.utc)
-                                duration_sec = cek_farming_response['farmingDurationInSec']
-                                end_time = started_at + timedelta(seconds=duration_sec)
-                                time_now = datetime.now(timezone.utc)
-
-                                remaining_time = end_time - time_now
-                                if remaining_time.total_seconds() > 0:
-                                    hours, remainder = divmod(remaining_time.total_seconds(), 3600)
-                                    minutes, _ = divmod(remainder, 60)
-                                    print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Claim farming in {int(hours)} hours {int(minutes)} minutes", flush=True)
-                                else:
-                                    print(Fore.GREEN + Style.BRIGHT + f"\r[ Farming ] : Farming can be claimed now", flush=True)
-                        elif farming_response['error']['message'] == "Farming didn't start":
-                            print(Fore.YELLOW + Style.BRIGHT + f"\r[ Farming ] : Starting Farming..", end="", flush=True)
-                            time.sleep(2)
-                            start_farming_response = start_farming(token)
-                            if start_farming_response is not None:
-                                print(Fore.GREEN + Style.BRIGHT + f"\r[ Farming ] : Started | Reward : {int(start_farming_response['farmingReward']):,}".replace(',', '.'), flush=True)
-                            else:
-                                if 'error' in start_farming_response:
-                                    if start_farming_response['error']['message'] == "Farming already started":
-                                        print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Farming Already Started", flush=True)
-                                else:
-                                    print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Gagal Start Farming", flush=True)
-                        else:
-                            print(f"\r[ Farming ] : {farming_response['error']['message']}", flush=True)
+        response = get_access_token_and_info(query_line)
+        if response is None:
+            continue
+        access_token = response['access_token']
+        user_info = response['user']
+        username = user_info['username']
+        user_id = user_info['id']
+        print(f"Xin chào, {username}! ID của bạn là {user_id}.")
+        while True:
+            farming_info = cek_farming(access_token)
+            if 'error' in farming_info:
+                if farming_info['error']['message'] == 'Farming is not running':
+                    start_response = start_farming(access_token)
+                    if 'error' in start_response:
+                        print(Fore.RED + Style.BRIGHT + f"\r[ Khởi động Farming ] : Lỗi khi khởi động farming. {start_response['error']['message']}", flush=True)
                     else:
-                        print(Fore.GREEN + Style.BRIGHT + f"\r[ Farming ] : Claimed | Balance: {int(farming_response['balance']):,}".replace(',', '.'), flush=True)
-                        print(Fore.YELLOW + Style.BRIGHT + f"\r[ Farming ] : Checking Farming..", end="", flush=True)
-                        time.sleep(2)
-                        cek_farming_response = cek_farming(token)
-                        if cek_farming_response is not None:
-                                if cek_farming_response['activeFarmingStartedAt'] is None:
-                                    print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Farming not started", flush=True)
-                                    print(Fore.YELLOW + Style.BRIGHT + f"\r[ Farming ] : Starting Farming..", end="", flush=True)
-                                    time.sleep(2)
-                                    start_farming_response = start_farming(token)
-                                    if start_farming_response is not None:
-                                        print(Fore.GREEN + Style.BRIGHT + f"\r[ Farming ] : Started | Reward : {int(start_farming_response['farmingReward']):,}".replace(',', '.'), flush=True)
-                                    else:
-                                        if 'error' in start_farming_response:
-                                            if start_farming_response['error']['message'] == "Farming already started":
-                                                print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Farming Already Started", flush=True)
-                                        else:
-                                            print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Gagal Start Farming", flush=True)
-                                else:
-                                    print(Fore.YELLOW + Style.BRIGHT + f"\r[ Farming ] : Farming Already Started", flush=True)                              
+                        print(Fore.GREEN + Style.BRIGHT + "\r[ Khởi động Farming ] : Khởi động farming thành công.", flush=True)
                 else:
-                    print(Fore.RED + Style.BRIGHT + f"\r[ Farming ] : Gagal Cek Farming", flush=True)
-                    continue
+                    print(Fore.RED + Style.BRIGHT + f"\r[ Kiểm tra Farming ] : Lỗi khi kiểm tra farming. {farming_info['error']['message']}", flush=True)
+            else:
+                finish_response = finish_farming(access_token)
+                if 'error' in finish_response:
+                    print(Fore.RED + Style.BRIGHT + f"\r[ Kết thúc Farming ] : Lỗi khi kết thúc farming. {finish_response['error']['message']}", flush=True)
+                else:
+                    print(Fore.GREEN + Style.BRIGHT + "\r[ Kết thúc Farming ] : Kết thúc farming thành công.", flush=True)
 
-            print(Fore.BLUE + Style.BRIGHT + f"\n==========SEMUA AKUN TELAH DI PROSES==========\n",  flush=True)    
-            animated_loading(300)            
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            if cek_task_enable == 'y':
+                tasks = cek_task(access_token)
+                for task in tasks:
+                    task_id = task['id']
+                    task_status = task['status']
+                    if task_status == 'pending':
+                        claim_response = claim_task(access_token, task_id)
+                        if 'error' in claim_response:
+                            print(Fore.RED + Style.BRIGHT + f"\r[ Nhận Nhiệm vụ ] : Lỗi khi nhận nhiệm vụ. {claim_response['error']['message']}", flush=True)
+                        else:
+                            print(Fore.GREEN + Style.BRIGHT + f"\r[ Nhận Nhiệm vụ ] : Nhận nhiệm vụ thành công. ID Nhiệm vụ: {task_id}", flush=True)
+                    elif task_status == 'completed':
+                        submit_response = submit_task(access_token, task_id)
+                        if 'error' in submit_response:
+                            print(Fore.RED + Style.BRIGHT + f"\r[ Nộp Nhiệm vụ ] : Lỗi khi nộp nhiệm vụ. {submit_response['error']['message']}", flush=True)
+                        else:
+                            print(Fore.GREEN + Style.BRIGHT + f"\r[ Nộp Nhiệm vụ ] : Nộp nhiệm vụ thành công. ID Nhiệm vụ: {task_id}", flush=True)
+            if cek_upgrade_enable == 'y':
+                auto_upgrade(access_token)
+            animated_loading(300)  # Delay antara setiap farming 5 phút
 
 if __name__ == "__main__":
-    main()
+    init(autoreset=True)
+    file_path = 'query.txt'  # Ganti dengan path file query Anda
+    run_bot(file_path)
